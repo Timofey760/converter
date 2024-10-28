94cba9dcec62fe42386b426ebb7e47bbb5e84a42
Your Name <you@example.com>
2024-10-28 20:19:26 +0300

    new version

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function convert(){
+function convert()
+{
+   let elementH1=document.getElementById('captionConverter')
+   console.log(elementH1.innerHTML)
+   let fromValue=+document.getElementById('inputFrom').value
+   let to=document.getElementById('inputTo');
+   let res;
+   if (elementH1.innerHTML=='метры в сантиметры')  res=fromValue*100;
+   if (elementH1.innerHTML=='километры в метры')  res=fromValue*1000;
+   if (elementH1.innerHTML=='дюймы в сантиметры')  res=fromValue*2.54;
+   if (elementH1.innerHTML=='мили в километры')  res=fromValue*1.609;
+   to.value=res;
+}
+
+/*
+function convert1(){
    let elementInputMetr, elementInputSm;
    elementInputMetr=document.getElementById("input_metr")
    elementInputSm=document.getElementById("input_sm")
@@ -29,10 +44,18 @@ function convert2(){
    let m=elementInputMetr.value
    elementInputSm.value=m*1.609
 }
-
+*/
  function load(){
     convert()
-    convert2()
-    convert3()
-    convert4()
+    //convert2()
+    //convert3()
+    //convert4()
+ }
+
+ function changeConverter()
+ {
+   //console.log(document.getElementById('captionConverter').innerHTML);
+   //console.log(document.getElementById('selectConverter').value);
+   document.getElementById('captionConverter').textContent=document.getElementById('selectConverter').value;
+   convert();
  }
